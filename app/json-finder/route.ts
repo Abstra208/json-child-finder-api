7374fb9f7f696764f9f70b01ccd02d23a3c8dd4d
Ludovic Morin <LudovicMorin357@hotmail.com>
2024-07-08 04:16:56 +0000

    request function test

--- app/json-finder/route.ts
+++ app/json-finder/route.ts
@@ -1,22 +1,7 @@
 export async function GET() {
-    return new Response("Hello, World!");
+    return new Response("Get request received");
 }
 
 export async function POST() {
-    try {
-        const response = await fetch("https://json-finder.vercel.app/json-finder", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ key: "value" })
-        });
-
-        if (!response.ok) {
-            throw new Error("Request failed");
-        }
-
-        const data = await response.json();
-        return new Response(JSON.stringify(data));
-    };
+    return new Response("Post request received!");
 }
